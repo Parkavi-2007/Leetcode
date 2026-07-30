2144e9f31b11cc58f63931cb7d11ee3eec569fd8
Parkavi-2007 <birava2233@gmail.com>
2026-07-30 23:39:21 +0530

    Time: 0 ms (100%), Space: 42.9 MB (70.12%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,14 +1,19 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String [] str=s.split(" ");
-        String word=str[0];
-         for(int i=0;i<str.length;i++)
-         {
-            if(i<str.length)
+        s=s.trim();
+        int count=0;
+        for(int i=s.length()-1;i>=0;i--)
+        {
+            char c=s.charAt(i);
+            if(c==' ')
             {
-             word=str[i];
+                break;
             }
-         }
-         return word.length();
+            else
+            {
+                count++;
+            }
+        }
+        return count;
     }
 }
